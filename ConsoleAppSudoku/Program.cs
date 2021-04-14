dfb4bb25c9dd686940ca4476d31d21460b73b6ee
marcos matos <marcoscaiobel@gmai.com>
2021-04-14 09:34:14 -0300

    fatornado

--- ConsoleAppSudoku/Program.cs
+++ ConsoleAppSudoku/Program.cs
@@ -7,9 +7,10 @@ namespace ConsoleAppSudoku
     {
         static void Main(string[] args)
         {
-            int contaLinha = 0 ,contaColuna = 0;
+            int contaLinha = 0, contaColuna = 0;
             string[,] matrix = new string[9, 9];
             string[] coluna = new string[9];
+            string[] linhaQuadrante = new string[9];
             while (true)
             {
                 if (contaLinha == 9)
@@ -21,14 +22,53 @@ namespace ConsoleAppSudoku
 
                 if (EhLinhaInvalida(linhaSeparada))
                 {
-                    Console.WriteLine("NAO");
+                    Console.Clear();
+                    Mensagem("NAO");
                     break;
                 }
 
                 AdicionaLinhaNaMatriz(contaLinha, matrix, linhaSeparada);
                 contaLinha++;
             }
-            LerColuna(contaColuna, matrix, coluna);
+            while (true)
+            {
+                if (contaLinha != 9)
+                {
+                    break;
+                }
+                LerColuna(contaColuna, matrix, coluna);
+                
+                MontaQuadrante0(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante1(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante2(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante3(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante4(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante5(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante6(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante7(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                MontaQuadrante8(matrix, linhaQuadrante);
+                if (EhQuadranteInvalida(linhaQuadrante))
+                    break;
+                Console.Clear();
+                Mensagem("SIM");
+                break;
+            }
 
             ////plota matriz
             //for (int i = 0; i < 9; i++)
@@ -43,25 +83,152 @@ namespace ConsoleAppSudoku
             Console.ReadLine();
         }
 
+        private static void MontaQuadrante0(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante= 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+                
+            }
+            
+        }
+        private static void MontaQuadrante1(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 3; j < 6; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+                
+            }
+
+        }
+        private static void MontaQuadrante2(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 6; j < 9; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+            }
+
+        }
+        private static void MontaQuadrante3(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 3; i < 6; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+            }
+
+        }
+        private static void MontaQuadrante4(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 3; i < 6; i++)
+            {
+                for (int j = 3; j < 6; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+            }
+
+        }
+        private static void MontaQuadrante5(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 3; i < 6; i++)
+            {
+                for (int j = 6; j < 9; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+            }
+
+        }
+        private static void MontaQuadrante6(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 6; i < 9; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+            }
+
+        }
+        private static void MontaQuadrante7(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 6; i < 9; i++)
+            {
+                for (int j = 3; j < 6; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+            }
+
+        }
+        private static void MontaQuadrante8(string[,] matrix, string[] quadrante)
+        {
+            int indiceLinhaQuadrante = 0;
+            for (int i = 6; i < 9; i++)
+            {
+                for (int j = 6; j < 9; j++)
+                {
+                    quadrante[indiceLinhaQuadrante] = matrix[i, j];
+                    indiceLinhaQuadrante++;
+                }
+            }
+
+        }
+
         private static void LerColuna(int contaColuna, string[,] matrix, string[] coluna)
         {
-            while(contaColuna<9)
+            while (contaColuna < 9)
             {
-                for (int i = 0; i < 9; i++)
-                {
-                    for (int j = contaColuna; j <= contaColuna; j++)
-                    {
-                        coluna[i] = matrix[i, j];
-                    }
-                }
+                MontaColuna(contaColuna, matrix, coluna);
+
                 if (EhColunaInvalida(coluna))
                 {
-                    Console.WriteLine("NAO");
+                    Console.Clear();
+                    Mensagem("NAO");
                     break;
                 }
                 contaColuna++;
             }
-            
+
+        }
+
+        private static void MontaColuna(int contaColuna, string[,] matrix, string[] coluna)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = contaColuna; j <= contaColuna; j++)
+                {
+                    coluna[i] = matrix[i, j];
+                }
+            }
         }
 
         private static void AdicionaLinhaNaMatriz(int contaLinha, string[,] matrix, string[] linhaSeparada)
@@ -73,8 +240,16 @@ namespace ConsoleAppSudoku
                     matrix[i, j] = linhaSeparada[j];
                 }
             }
-            
+
         }
+
+        private static void Mensagem(string mensagem)
+        {
+            Console.WriteLine("---");
+            Console.WriteLine(mensagem);
+            Console.WriteLine("---");
+        }
+
 
         private static bool EhLinhaInvalida(string[] separaLinha)
         {
@@ -84,6 +259,11 @@ namespace ConsoleAppSudoku
         {
             return coluna.Length - coluna.ToList().Distinct().Count() != 0;
         }
-
+        private static bool EhQuadranteInvalida(string[] quadrante)
+        {
+            Console.Clear();
+            Mensagem("NAO");
+            return quadrante.Length - quadrante.ToList().Distinct().Count() != 0;
+        }
     }
 }
